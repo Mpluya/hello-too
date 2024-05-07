e8f3e9227f736174d1412d94873c9431f3a5f354
Mae Large <mlarge@broadcom.com>
2024-05-07 06:49:57 -0700

    nudge

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu 6!", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu 7!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu 6!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu 7!"));
     }
 }
