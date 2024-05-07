e600c5fcb53daf622febee2f82fcfc45a015d719
Mae Large <mlarge@broadcom.com>
2024-05-07 08:40:15 -0700

    nudge

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu 14!";
+		return "Greetings from Spring Boot + Tanzu 15!";
 	}
 
 }
